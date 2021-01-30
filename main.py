543d2bdbc9e780de02e79a8a6920941c1d067f1a
Jay <jfranko13@icloud.com>
2021-01-30 12:36:02 -0500

    fx comments

--- main.py
+++ main.py
@@ -10,6 +10,19 @@ def main():
 def checkCurrentBestHand():
     print("This Function evaluates what the current best hand is based on the current river and your cards\n")
 
+def checkIfRoyalFlush():
+    print("This Function evaluates if the current hand is a royal flush based on the current river and your cards\n")
 
+def checkIfStraightFlush():
+    print("This Function evaluates if the current hand is a straight flush based on the current river and your cards\n")
+
+def checkIfFourOfAKind():
+    print("This Function evaluates if the current hand is a four of a kind based on the current river and your cards\n")
+
+def checkIfFullHouse():
+    print("This Function evaluates if the current hand is a full house based on the current river and your cards\n")
+
+def checkIfFlush():
+    print("This Function evaluates if the current hand is a flush based on the current river and your cards\n")
 
 main()
