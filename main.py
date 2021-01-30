284a82b93c32e3e73ea898c9eb8f09d2e3125bb0
Kspraker <ksprake@clemson.edu>
2021-01-30 12:40:25 -0500

    added the other 5 possible hands

--- main.py
+++ main.py
@@ -10,6 +10,20 @@ def main():
 def checkCurrentBestHand():
     print("This Function evaluates what the current best hand is based on the current river and your cards\n")
 
+def checkStraight():
+    print("This function evaluates if the player has a straight")
+
+def checkThree():
+    print("This function evaluates if the player has three of a kind")
+
+def checkTwoPair():
+    print("This function evaluates if the player has two pairs")
+
+def checkPair():
+    print("This function evaluates if the player has one pair")
+
+def checkHighCard():
+    print("This function evaluates if the player only has high card")
 
 
 main()
