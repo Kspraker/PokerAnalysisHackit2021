f8009bb538905cf6f2dbf16e6d2a4ce53e934e7c
Kspraker <ksprake@clemson.edu>
2021-01-30 18:42:01 -0500

    i dont remember what i've done

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ card = {
 }
 
 class Card:
-    def __init__(self, value, color):
+    def __init__(self, value, suit):
         self.value = value
-        self.color = color
+        self.suit = suit
 
 community = list()
 
-colors = ['Hearts', 'Diamonds', 'Spades', 'Clubs']
-
-deck = [Card(value, color) for value in range(1, 14) for color in colors]
+suits = [0, 1, 2, 3]
+# Hearts, Diamonds, Club, Spade 
+deck = [Card(value, suit) for value in range(1, 14) for suit in suits]
 
 #river.append(Card("heart", "2"))
 
@@ -24,7 +24,8 @@ card = {
 
 deckSize = 52
 
-inPlay = [Card(2, 'Hearts'), Card(3, 'Diamonds'), Card(11, 'Clubs')]
+inPlay = [Card(11, 1), Card(10, 1), Card(9, 1), Card(8, 1), Card(7, 1)]
+sortedSuits = inPlay
 
 def main():
     print("Hello, this is program a Texas Holdem Game Analysis\n")
@@ -41,6 +42,9 @@ def main():
     
     #checkHighCard()
     sortCards()
+    sortSuits()
+
+    checkIfFlush()
 
 
     #playAgain = input("Do you want to play again? ")
@@ -48,15 +52,22 @@ def sortCards():
     sorted = list()
     inPlay.sort(key = lambda x: x.value, reverse = True)
 
+def sortSuits():
+    sorted = list()
+    sortedSuits.sort(key = lambda x: x.suit, reverse = True)
 
 def checkCurrentBestHand():
     print("This Function evaluates what the current best hand is based on the current river and your cards\n")
 
 def checkIfRoyalFlush():
     print("This Function evaluates if the current hand is a royal flush based on the current river and your cards\n")
+    #if checkIfFlush()
+    #    return true
 
 def checkIfStraightFlush():
     print("This Function evaluates if the current hand is a straight flush based on the current river and your cards\n")
+    # if checkIfStraight() and checkIfFlush()
+    #     return true
 
 def checkIfFourOfAKind():
     print("This Function evaluates if the current hand is a four of a kind based on the current river and your cards\n")
@@ -66,12 +77,56 @@ def checkIfFullHouse():
 
 def checkIfFlush():
     print("This Function evaluates if the current hand is a flush based on the current river and your cards\n")
+    total = 1
+    
+    for i in range(len(sortedSuits)-1):
+        if total == 5:
+            break
+        if sortedSuits[i].suit == sortedSuits[i+1].suit:
+            total = total + 1
+        else:
+            total = 1
+
+    print(total)
 
 def checkStraight():
+    # Remember to make case for ace, since it can begin and end
     print("This function evaluates if the player has a straight\n")
+    flag = True
+    count = 1
+
+    for i in range(len(inPlay)-1):
+        if(inPlay[i+1] != None):
+            if inPlay[i].value - 1 == inPlay[i+1].value:
+                count = count + 1
+
+            else:
+                count = 0
+
+    print(count)
+
+
+
+
+
 
 def checkThree():
     print("This function evaluates if the player has three of a kind\n")
+    total = 0
+ 
+    # probably messed up the indexing here, not starting at 0
+    # current = inPlay[0].value
+    for i in range(len(inPlay)-1):
+        if total == 2:
+            break
+        if inPlay[i].value == inPlay[i+1].value:
+            total = total + 1
+        else:
+            total = 0
+
+    print(total)
+
+
 
 def checkTwoPair():
     print("This function evaluates if the player has two pairs\n")
